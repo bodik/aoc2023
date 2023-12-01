89d4c25d736d36c8f7670b43a6e583a5cb3a9cb3
Radoslav Bodó <bodik@cesnet.cz>
2023-12-01 11:07:31 +0100

    ruff and rfind for fun

--- day01/part2.py
+++ day01/part2.py
@@ -4,48 +4,40 @@ import sys
 
 def firstlast(item):
     data = list(filter(lambda x: x.isnumeric(), item))
-    return int(f'{data[0]}{data[-1]}')
+    return int(f"{data[0]}{data[-1]}")
 
 
 trans = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9',
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
 }
 
 data = []
 
 data1 = pathlib.Path(sys.argv[1]).read_text().splitlines()
-#print(data1)
+# print(data1)
 
 for item in data1:
     orig = item
 
-    nums = sorted(
-        filter(
-            lambda x: x[0] > -1,
-            [(item.find(y), y) for y in trans.keys()]
-        )
-    )
-    if nums:
+    if nums := sorted(
+        filter(lambda x: x[0] > -1, [(item.find(y), y) for y in trans.keys()])
+    ):
         item = item.replace(nums[0][1], trans[nums[0][1]])
 
-    nums = sorted(
-        filter(
-            lambda x: x[0] > -1,
-            [(item[::-1].find(y[::-1]), y[::-1]) for y in trans.keys()]
-        )
-    )
-    if nums:
-        item = item[::-1].replace(nums[0][1], trans[nums[0][1][::-1]])[::-1]
+    if nums := sorted(
+        filter(lambda x: x[0] > -1, [(item.rfind(y), y[::-1]) for y in trans.keys()])
+    ):
+        item = item[::-1].replace(nums[-1][1], trans[nums[-1][1][::-1]])[::-1]
 
-    print(orig, '===', item, '===', firstlast(item))
+    print(orig, "===", item, "===", firstlast(item))
     data.append(firstlast(item))
 
 print(sum(data))
